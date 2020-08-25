a13b595bda9636e4a26ffef836dc3b9ca6add335
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-25 20:08:47 -0300

    Agrego ejercicios

---
+++ index.js
@@ -0,0 +1,8 @@
+const parrafos = document.getElementsByTagName("p")
+console.log(parrafos)
+
+const elementosVerdes = document.getElementsByClassName("verde")
+console.log(elementosVerdes)
+
+const primerElementoBordeRojo = document.querySelector("#borde-rojo")
+console.log(primerElementoBordeRojo)
