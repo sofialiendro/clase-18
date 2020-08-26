2c3006376c13592e5acd4addb14e0db93fd156ef
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-25 22:47:29 -0300

    Ejercicios

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const parrafos = document.getElementsByTagName("p")
-console.log(parrafos)
+// const parrafos = document.getElementsByTagName("p")
+// console.log(parrafos)
 
-const elementosVerdes = document.getElementsByClassName("verde")
-console.log(elementosVerdes)
+// const elementosVerdes = document.getElementsByClassName("verde")
+// console.log(elementosVerdes)
 
-const primerElementoBordeRojo = document.querySelector("#borde-rojo")
-console.log(primerElementoBordeRojo)
+// const primerElementoBordeRojo = document.querySelector("#borde-rojo")
+// console.log(primerElementoBordeRojo)
+
+
+
+const claseVerde = document.querySelector(".verde")
+claseVerde.classList.remove("verde")
+console.log(claseVerde)
+
+const claseRojo = document.querySelector(".rojo")
+claseRojo.classList.add("rosa")
+console.log(claseRojo)
+
+const claseRosa = document.querySelector(".rosa")
+console.log(claseRosa.classList.toggle("rosa"))
+
+const claseAmarillo = document.querySelector(".amarillo")
+console.log(claseAmarillo.classList.contains(".rojo"))
